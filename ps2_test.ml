a6e5b1648ea5dcf81690b051a93ab13ca862531d
Suraj Anandalwar <sda37@cornell.edu>
2014-09-16 02:14:12 -0400

    count_ops make_fact_tree

--- ps2_test.ml
+++ ps2_test.ml
@@ -1,6 +1,32 @@
 open ps2
 open Assertions	
 
+let a = Binop ((+) ,
+                Val 3,
+                Unop ((~ -) ,
+                      Binop ((/) ,
+                             Val 5,
+                             Val 2)))
+
+let b = Val 3
+
+TEST_UNIT "count_ops_test1" = assert_true (count_ops t = 3)
+TEST_UNIT "count_ops_test1" = assert_true (count_ops b = 0)
+
+let c = Binop(( * ) 
+              Val 3,
+              Binop (( * ) , 
+              	     Val 2 ,
+                     Binop (( * ) , 
+                            Val 1 ,  
+                            Val 1)))
+
+TEST_UNIT "make_fact_tree_test1" = assert_true (make_fact_tree 3 = c)
+TEST_UNIT "make_fact_tree_test1" = assert_true (make_fact_tree 0 = Val 1)
+TEST_UNIT "make_fact_tree_test1" = assert_true (make_fact_tree 1 = Binop(( * ), Val 1, Val 1))
+
+
+
 TEST_UNIT "product_test1" = assert_true (product [4.;5.;6.] = 120.)
 TEST_UNIT "product_test1" = assert_true (product [] = 1.)
 TEST_UNIT "product_test1" = assert_true (product [0.;5.] = 0.)
@@ -17,6 +43,7 @@ TEST_UNIT "concat_right_test1" = assert_true (concat_right [] = "")
 TEST_UNIT "concat_right_test1" = assert_true (concat_right ["nikita";""] = "nikita")
 TEST_UNIT "concat_right_test1" = assert_true (concat_right ["nikita";" gupta";" is";" cool"] = "nikita gupta is cool")
 TEST_UNIT "concat_right_test1" = assert_false (concat_right ["nikita";"gupta"] = "nikitagupt")
+
 
 
 
