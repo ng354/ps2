8e2ef2946ec88a3b952363a2c6bb7f4b5235b35a
Suraj Anandalwar <sda37@cornell.edu>
2014-09-18 17:45:43 -0400

    update

--- ps2_test.ml
+++ ps2_test.ml
@@ -1,9 +1,10 @@
 open ps2
-open Assertions	
+open Assertions
+
 
 let a = Binop ((+) ,
                 Val 3,
-                Unop ((~ -) ,
+                Unop ((~-) ,
                       Binop ((/) ,
                              Val 5,
                              Val 2)))
@@ -11,9 +12,9 @@ let a = Binop ((+) ,
 let b = Val 3
 
 TEST_UNIT "count_ops_test1" = assert_true (count_ops t = 3)
-TEST_UNIT "count_ops_test1" = assert_true (count_ops b = 0)
+TEST_UNIT "count_ops_test2" = assert_true (count_ops b = 0)
 
-let c = Binop(( * ) 
+let c = Binop(( * ) ,
               Val 3,
               Binop (( * ) , 
               	     Val 2 ,
@@ -22,27 +23,91 @@ let c = Binop(( * )
                             Val 1)))
 
 TEST_UNIT "make_fact_tree_test1" = assert_true (make_fact_tree 3 = c)
-TEST_UNIT "make_fact_tree_test1" = assert_true (make_fact_tree 0 = Val 1)
-TEST_UNIT "make_fact_tree_test1" = assert_true (make_fact_tree 1 = Binop(( * ), Val 1, Val 1))
+TEST_UNIT "make_fact_tree_test2" = assert_true (make_fact_tree 0 = Val 1)
+TEST_UNIT "make_fact_tree_test3" = assert_true (make_fact_tree 1 = Binop(( * ), Val 1, Val 1))
+
+let d = Binop(( @ ) ,
+               Val [2;3],
+               Binop(( @ ) ,
+                     Val [4;5],
+                     Unop((List.rev) ,
+                          Binop(( @ ) ,
+                                Val [6;7],
+                                Val [8;9]))))
+
+
+TEST_UNIT "eval_test1" = assert_true (eval a = 1)
+TEST_UNIT "eval_test1" = assert_true (eval b = 3)
+TEST_UNIT "eval_test1" = assert_true (eval c = 6)
+TEST_UNIT "eval_test1" = assert_true (eval d = [2;3;4;5;9;8;7;6])
+
 
 
 
 TEST_UNIT "product_test1" = assert_true (product [4.;5.;6.] = 120.)
-TEST_UNIT "product_test1" = assert_true (product [] = 1.)
-TEST_UNIT "product_test1" = assert_true (product [0.;5.] = 0.)
-TEST_UNIT "product_test1" = assert_false (product [1.;3.5;4.] = 7.)
+TEST_UNIT "product_test2" = assert_true (product [] = 1.)
+TEST_UNIT "product_test3" = assert_true (product [0.;5.] = 0.)
+TEST_UNIT "product_test4" = assert_false (product [1.;3.5;4.] = 7.)
 
 TEST_UNIT "concat_left_test1" = assert_true (concat_left ["nikita";"gupta"] = "nikitagupta")
-TEST_UNIT "concat_left_test1" = assert_true (concat_left [] = "")
-TEST_UNIT "concat_left_test1" = assert_true (concat_left ["nikita";""] = "nikita")
-TEST_UNIT "concat_left_test1" = assert_true (concat_left ["nikita";" gupta";" is";" cool"] = "nikita gupta is cool")
-TEST_UNIT "concat_left_test1" = assert_false (concat_left ["nikita";"gupta"] = "nikitagupt")
+TEST_UNIT "concat_left_test2" = assert_true (concat_left [] = "")
+TEST_UNIT "concat_left_test3" = assert_true (concat_left ["nikita";""] = "nikita")
+TEST_UNIT "concat_left_test4" = assert_true (concat_left ["nikita";" gupta";" is";" cool"] = "nikita gupta is cool")
+TEST_UNIT "concat_left_test5" = assert_false (concat_left ["nikita";"gupta"] = "nikitagupt")
 
 TEST_UNIT "concat_right_test1" = assert_true (concat_right ["nikita";"gupta"] = "nikitagupta")
-TEST_UNIT "concat_right_test1" = assert_true (concat_right [] = "")
-TEST_UNIT "concat_right_test1" = assert_true (concat_right ["nikita";""] = "nikita")
-TEST_UNIT "concat_right_test1" = assert_true (concat_right ["nikita";" gupta";" is";" cool"] = "nikita gupta is cool")
-TEST_UNIT "concat_right_test1" = assert_false (concat_right ["nikita";"gupta"] = "nikitagupt")
+TEST_UNIT "concat_right_test2" = assert_true (concat_right [] = "")
+TEST_UNIT "concat_right_test3" = assert_true (concat_right ["nikita";""] = "nikita")
+TEST_UNIT "concat_right_test4" = assert_true (concat_right ["nikita";" gupta";" is";" cool"] = "nikita gupta is cool")
+TEST_UNIT "concat_right_test5" = assert_false (concat_right ["nikita";"gupta"] = "nikitagupt")
+
+let wc1 = WCpat 
+let wc2 = a
+let wc3 = a
+let var1 = VarPat "hello"
+let var2 = d;; let var3 = d
+let var4 = VarPat "world2"
+let var5 = "Suraj95"
+let tuple0 = TuplePat []
+let tuple1 = TuplePat [wc1;wc2;wc3]
+let tuple2 = TuplePat [var1;var2;var3]
+let tuple3 = TuplePat [var1;var4;var5]
+let tuple4 = TuplePat [wc1;var1;wc2;var2;wc3;var5]
+let tuple5 = TuplePat [var1;wc1;var4;wc2;var5;wc3]
+let structor1 = StructorPat ("1",tuple4)
+
+
+TEST_UNIT "count_wcs0" = assert_true (count_wcs tuple0 = 0)
+TEST_UNIT "count_wcs1" = assert_true (count_wcs tuple1 = 3)
+TEST_UNIT "count_wcs2" = assert_true (count_wcs structor1 = 3)
+TEST_UNIT "count_wcs3" = assert_true (count_wcs wc1 = 1)
+
+
+
+TEST_UNIT "count_wcs_and_var_lengths1" = assert_true (count_wcs_and_var_lengths tuple0 = 0)
+TEST_UNIT "count_wcs_and_var_lengths2" = assert_true (count_wcs_and_var_lengths tuple1 = 3)
+TEST_UNIT "count_wcs_and_var_lengths3" = assert_true (count_wcs_and_var_lengths tuple2 = 15)
+TEST_UNIT "count_wcs_and_var_lengths2" = assert_true (count_wcs_and_var_lengths tuple4 = 20)
+TEST_UNIT "count_wcs_and_var_lengths2" = assert_true (count_wcs_and_var_lengths structor1 = 20)
+
+TEST_UNIT "count_var0" = assert_true (count_var "world" tuple0 = 0)
+TEST_UNIT "count_var1" = assert_true (count_var "world" tuple1 = 0)
+TEST_UNIT "count_var2" = assert_true (count_var "hello" tuple1 = 3)
+TEST_UNIT "count_var3" = assert_true (count_var "hello" tuple4 = 2)
+TEST_UNIT "count_var4" = assert_true (count_var "hello" structor1 = 2)
+
+TEST_UNIT "extract_names4" = assert_true (extract_names tuple0 = [])
+TEST_UNIT "extract_names4" = assert_true (extract_names tuple1 = [])
+TEST_UNIT "extract_names4" = assert_true (extract_names tuple3 = ["world2";"Suraj95"])
+TEST_UNIT "extract_names4" = assert_true (extract_names tuple4 = ["hello";"hello";"Suraj95"])
+TEST_UNIT "extract_names4" = assert_true (extract_names structor1 = ["hello";"hello";"Suraj95"])
+
+TEST_UNIT "all_vars_unique0" = assert_false (all_vars_unique tuple0)
+TEST_UNIT "all_vars_unique1" = assert_false (all_vars_unique tuple1)
+TEST_UNIT "all_vars_unique2" = assert_true (all_vars_unique tuple3)
+TEST_UNIT "all_vars_unique3" = assert_false (all_vars_unique tuple4)
+TEST_UNIT "all_vars_unique4" = assert_false (all_vars_unique structor1)
+TEST_UNIT "all_vars_unique5" = assert_true (all_vars_unique tuple5)
 
 
 
